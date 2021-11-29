08ca086b5c03770456bf65d848ccd245cfa65edf
SheridanB <sheridanb2269@gmail.com>
2021-11-29 15:52:57 -0600

    first commit

---
+++ src/main/java/com/techtalentsouth/BlogPosts/BlogPostRepository.java
@@ -0,0 +1,7 @@
+package com.techtalentsouth.BlogPosts;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository <BlogPost, Long>{
+    
+}
